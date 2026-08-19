630c7b0ddc9c1edde348f77a2da74238eedcc379
marcospalvestec-jpg <marcos.palvestec@gmail.com>
2026-08-19 15:20:11 -0300

    Adiciona atividade 6 de Python

---
+++ Atividade 6.py
@@ -0,0 +1,17 @@
+# Problema: "O triplo de um número menos 5 é igual ao dobro do mesmo número mais 1."
+# Equação: 3x - 5 = 2x + 1
+
+
+#Resolvendo a equação:
+#3x - 5 = 2x + 1
+#x = 6
+numero = 6
+
+# Calculando cada lado da equação para verificação
+lado_esquerdo = 3 * (numero) - 5
+lado_direito = 2 * (numero) + 1
+
+print(f" O número que atende a regra é {numero}.")
+print(f"Verificação: ")
+print(f"O tripo de um número é: {lado_esquerdo}")
+print(f"O dobro de um número + 1 é: {lado_direito}")
